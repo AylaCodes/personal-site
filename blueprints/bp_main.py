9035b8918beb7cc2754689d56e996cc8b72f92aa
AylaCodes <Luckaylawoo@gmail.com>
2022-11-30 07:50:52 +0000

    About route can be in main blueprint

--- blueprints/bp_main.py
+++ blueprints/bp_main.py
@@ -1,10 +1,12 @@
 import json
 
+import arrow
+
 from pathlib import Path
 
 from sanic import Blueprint
 from sanic.request import Request
-from sanic.response import HTTPResponse, text, html
+from sanic.response import HTTPResponse, html
 from jinja2 import Environment, FileSystemLoader
 
 config = json.loads((Path("html/templates/main.json")).read_text())
@@ -12,6 +14,7 @@ config = json.loads((Path("html/templates/main.json")).read_text())
 # Jinja
 environment = Environment(loader=FileSystemLoader(Path("html/templates")))
 template_main = environment.get_template("main.jinja")
+template_about = environment.get_template("about.jinja")
 
 # Sanic
 main = Blueprint("main", host="wolfie.space")
@@ -19,3 +22,12 @@ main = Blueprint("main", host="wolfie.space")
 @main.route("/")
 async def home_main(request: Request) -> HTTPResponse:
     return html(template_main.render(active="home", **config))
+
+@main.route("/about")
+async def about_page(request: Request) -> HTTPResponse:
+    return html(template_about.render(
+            active="about",
+            birth_date=arrow.get(870262176).humanize(),
+            **config
+        )
+    )
